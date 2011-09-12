b3f87ab617e2bfe1d0b873155562543c7dd6611e
Robby Tarigan <robby.tarigan@gmail.com>
2011-09-12 23:38:15 +0100

    Creating and applying a theme.

---
+++ 70-515/ThemesLab/Default.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <asp:Image ID="Image1" SkinID="Logo" runat="server" />
+    <br />
+    <asp:TextBox ID="TextBoxUname" runat="server" CssClass="textBox"></asp:TextBox>
+    <br />
+    <asp:Button ID="ButtonSubmit" runat="server" Text="Hello" CssClass="button" OnClick="ButtonSubmit_Click" />
+</asp:Content>
---
+++ 70-515/ThemesLab/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_PreInit(object sender, EventArgs e) {
+        if (Session["theme"] != null) {
+            Page.Theme = (string)Session["theme"];
+        }
+    }
+    protected void ButtonSubmit_Click(object sender, EventArgs e) {
+        string theme = Page.Theme;
+        //switch themes
+        if (theme == "Contoso") {
+            Session["theme"] = "Fabrikam";
+        } else {
+            Session["theme"] = "Contoso";
+        }
+        //Page.Theme = (string)Session["theme"]; // Exception here
+        Server.Transfer(Request.Path);
+    }
+}
