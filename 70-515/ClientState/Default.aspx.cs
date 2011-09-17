148f44061b242baaa226c9865e0655c43d64782a
Robby Tarigan <robby.tarigan@gmail.com>
2011-09-17 22:29:28 +0100

    Storing data in a query string.

--- 70-515/ClientState/Default.aspx.cs
+++ 70-515/ClientState/Default.aspx.cs
@@ -9,15 +9,17 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        // Read the cookie clicks and increment
-        int cookieClicks;
-        if (Request.Cookies["clicks"] != null) {
-            cookieClicks = int.Parse(Request.Cookies["clicks"].Value) + 1;
-        } else {
-            cookieClicks = 1;
+        if (!IsPostBack) {
+            // Read the query string
+            int queryClicks;
+            if (Request.QueryString["clicks"] != null) {
+                queryClicks = int.Parse(Request.QueryString["clicks"]) + 1;
+            } else {
+                queryClicks = 1;
+            }
+            // Define the query string in the hyperlink
+            Hyperlink1.NavigateUrl += "?clicks=" + queryClicks.ToString();
+            Label1.Text = "Query clicks: " + queryClicks.ToString();
         }
-        // Save the cookie to be returned on the next visit
-        Response.Cookies["clicks"].Value = cookieClicks.ToString();
-        Label1.Text = "Cookie clicks: " + cookieClicks.ToString();
     }
 }
