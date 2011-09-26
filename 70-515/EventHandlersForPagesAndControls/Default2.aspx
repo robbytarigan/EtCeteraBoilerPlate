67e668d8969ac166e75ceed884b2b54e10aa4241
Robby Tarigan <robby.tarigan@gmail.com>
2011-09-26 23:50:40 +0100

    Dynamically add controls to a page with Master page.

---
+++ 70-515/EventHandlersForPagesAndControls/Default2.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Default2.aspx.cs" Inherits="Default2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Label ID="resultLabel" runat="server" />
+
+    <asp:PlaceHolder ID="containerPlaceHolder" runat="server"></asp:PlaceHolder>
+</asp:Content>
+
---
+++ 70-515/EventHandlersForPagesAndControls/Default2.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{    
+
+
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TextBox colorTextBox = new TextBox() { ID = "colorTextBox" };
+        Button submitButton = new Button() { ID="submitButton", Text = "Submit" };
+
+        submitButton.Click += new EventHandler(submitButton_Click);
+        containerPlaceHolder.Controls.Add(colorTextBox);
+        containerPlaceHolder.Controls.Add(submitButton);
+
+    }
+
+    void submitButton_Click(object sender, EventArgs e) {
+        TextBox colorTextBox = containerPlaceHolder.FindControl("colorTextBox") as TextBox;
+
+        if (colorTextBox != null) {
+            resultLabel.Text = String.Format("Posted {0} <br />", colorTextBox.Text);
+        }
+    }
+}
