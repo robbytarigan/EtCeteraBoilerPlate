2bae039aff6b85747800d7c83379b89d2856025f
Robby Tarigan <robby.tarigan@gmail.com>
2011-11-13 23:28:58 +0000

    Upload picture and read picture from specified folder.

--- 70-515/DisplayPictureThatUserUploads/Default.aspx
+++ 70-515/DisplayPictureThatUserUploads/Default.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="false" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+﻿<%@ Page Language="C#" AutoEventWireup="false" CodeFile="Default.aspx.cs" Inherits="_Default" EnableViewState="false" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
@@ -10,10 +10,22 @@
     <form id="form1" runat="server">
     <asp:Panel runat="server" ID="uploadPanel">  
         <asp:FileUpload ID="pictureFileUpload" runat="server" />
-        <asp:Button ID="uploadButton" runat="server" Text="Upload" />
+        <asp:Button ID="uploadButton" runat="server" Text="Upload" onclick="uploadButton_Click" />
     </asp:Panel>
     <asp:Panel runat="server" ID="displayPanel">
-        <asp:Image ID="pictureImage" runat="server" />
+        <asp:ListView ID="pictureListView" runat="server">
+            <LayoutTemplate>
+                <div>
+                    <div runat="server" id="itemPlaceHolder">
+                    </div>
+                </div>
+            </LayoutTemplate>
+            <ItemTemplate>
+                <div runat="server">
+                    <asp:Image ID="pictureImage" runat="server" ImageUrl='<%# Eval("Path") %>' AlternateText='<%# Eval("Text") %>' />
+                </div>
+            </ItemTemplate>
+        </asp:ListView>        
     </asp:Panel>
     </form>
 </body>
--- 70-515/DisplayPictureThatUserUploads/Default.aspx.cs
+++ 70-515/DisplayPictureThatUserUploads/Default.aspx.cs
@@ -4,11 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
+
+public class PictureItem {
+    public string Text { get; set; }
+    public string Path { get; set; }
+}
 
 public partial class _Default : System.Web.UI.Page
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
+{    
 
+    private const string PictureFolder = "picture";    
+
+    private string[] ImageContentTypes = new string[] { "image/gif", "image/jpeg", "image/pjpeg", "image/png", "image/svg+xml", "image/tiff", "image/vnd.microsoft.icon" };
+
+    private IEnumerable<PictureItem> GetPictureItems() {
+        foreach (string s in Directory.GetFiles(Server.MapPath(PictureFolder))) {
+            string fileName = Path.GetFileName(s);
+            yield return new PictureItem { Path = ResolveUrl(String.Format("~/{0}/{1}", PictureFolder, fileName)), Text = fileName };
+        }
+    }
+
+    protected override void OnLoadComplete(EventArgs e) {
+        base.OnLoadComplete(e);
+        pictureListView.DataSource = GetPictureItems();
+        pictureListView.DataBind();
+        
+    }
+
+    protected void uploadButton_Click(object sender, EventArgs e) {
+        if (ImageContentTypes.Contains(pictureFileUpload.PostedFile.ContentType.ToLowerInvariant())) {
+            pictureFileUpload.SaveAs(String.Format("{0}/{1}", Server.MapPath(PictureFolder), pictureFileUpload.FileName));
+            
+        }
+        
     }
 }
