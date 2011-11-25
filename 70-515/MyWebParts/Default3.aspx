5286a088628ffec6f2509481ddbb397bc816ea6f
Robby Tarigan <robby.tarigan@gmail.com>
2011-11-25 21:56:45 +0000

    Use ConnectionsZone control to enable user manually establish connections among GetName, GreetUser and ShowNameBackward controls.

---
+++ 70-515/MyWebParts/Default3.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default3.aspx.cs" Inherits="Default3" %>
+
+<%@ Register src="LogoWebPart.ascx" tagname="LogoWebPart" tagprefix="uc1" %>
+<%@ Register src="CalendarWebPart.ascx" tagname="CalendarWebPart" tagprefix="uc2" %>
+
+<%@ Register src="GetName.ascx" tagname="GetName" tagprefix="uc3" %>
+<%@ Register src="GreetUser.ascx" tagname="GreetUser" tagprefix="uc4" %>
+<%@ Register src="ShowNameBackwards.ascx" tagname="ShowNameBackwards" tagprefix="uc5" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>My Web Part Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:WebPartManager ID="WebPartManager1" runat="server"></asp:WebPartManager>
+
+    <div style="width: 700px">
+        <asp:WebPartZone ID="WebPartZoneTop" runat="server" HeaderText="Top Zone" Style="width: 700px; height: auto">
+            <ZoneTemplate>
+                <uc1:LogoWebPart ID="LogoWebPart1" runat="server" title="Logo" />
+            </ZoneTemplate>
+        </asp:WebPartZone>
+        <asp:WebPartZone ID="WebPartZoneLeft" runat="server" HeaderText="Left Zone" Style="width: 300px; float: left; height: 300px">
+            <ZoneTemplate>
+                <uc2:CalendarWebPart ID="CalendarWebPart1" runat="server" title="Calendar" />
+            </ZoneTemplate>
+        </asp:WebPartZone>        
+        <div style="width: 395px; float: right; height: auto; padding-left: 5px">
+            <asp:DropDownList ID="DropDownListModes" runat="server" AutoPostBack="true">
+            </asp:DropDownList>
+                    
+            <asp:EditorZone ID="EditorZone1" runat="server">
+                <ZoneTemplate>
+                    <asp:AppearanceEditorPart ID="AppearanceEditorPart1" runat="server">
+                    </asp:AppearanceEditorPart>
+                    <asp:LayoutEditorPart ID="LayoutEditorPart1" runat="server">
+                    </asp:LayoutEditorPart>
+                </ZoneTemplate>
+            </asp:EditorZone>
+
+            <asp:CatalogZone ID="CatalogZone1" runat="server">
+                <ZoneTemplate>
+                    <asp:DeclarativeCatalogPart ID="DeclarativeCatalogPart1" runat="server">
+                        <WebPartsTemplate>
+                            <uc1:LogoWebPart ID="LogoWebPart1" runat="server" title="Logo" />
+                            <uc2:CalendarWebPart ID="CalendarWebPart1" runat="server" title="Calendar" />
+                        </WebPartsTemplate>
+                    </asp:DeclarativeCatalogPart>                    
+                </ZoneTemplate>
+            </asp:CatalogZone>
+
+            <asp:ConnectionsZone ID="connectionsZone1" runat="server">
+                <cancelverb text="Terminate" />
+                <closeverb text="Close Zone" />
+                <configureverb text="Configure" />
+                <connectverb text="Connect Controls" />
+                <disconnectverb text="End Connection" />            
+            </asp:ConnectionsZone>
+        </div>               
+             
+        <asp:WebPartZone ID="WebPartZoneBottom" runat="server" HeaderText="Bottom Zone" Style="width: 700px; height: auto;">
+            <ZoneTemplate>
+                <uc3:GetName ID="GetName1" runat="server" title="Enter Name" />
+                <uc4:GreetUser ID="GreetUser1" runat="server" title="Greeting" />
+                <uc5:ShowNameBackwards ID="ShowNameBackwards1" runat="server" title="Backwards Name" />                
+            </ZoneTemplate>
+        </asp:WebPartZone>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ 70-515/MyWebParts/Default3.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+public partial class Default3 : System.Web.UI.Page
+{
+    void GenerateModeList(object sender, EventArgs e) {
+        WebPartManager _manager = WebPartManager.GetCurrentWebPartManager(Page);
+        String browseModeName = WebPartManager.BrowseDisplayMode.Name;
+        DropDownListModes.Items.Clear();
+        // Fill the drop-down list with the names of supported display modes.
+        foreach (WebPartDisplayMode mode in _manager.SupportedDisplayModes) {
+            String modeName = mode.Name;
+            // Make sure a mode is enabled before adding it.
+            if (mode.IsEnabled(_manager)) {
+                ListItem item = new ListItem(modeName, modeName);
+                DropDownListModes.Items.Add(item);
+            }
+        }
+        // Select the current mode
+        ListItemCollection items = DropDownListModes.Items;
+        int selectedIndex = items.IndexOf(items.FindByText(_manager.DisplayMode.Name));
+        DropDownListModes.SelectedIndex = selectedIndex;
+    }
+
+    void DropDownListModes_SelectedIndexChanged(object sender, EventArgs e) {
+        WebPartManager manager = WebPartManager.GetCurrentWebPartManager(Page);
+        WebPartDisplayMode mode = manager.SupportedDisplayModes[DropDownListModes.SelectedValue];
+        if (mode != null)
+            manager.DisplayMode = mode;
+    }
+
+    void Page_Init(object sender, EventArgs e) {
+        Page.InitComplete += new EventHandler(GenerateModeList);
+        DropDownListModes.SelectedIndexChanged += new EventHandler(DropDownListModes_SelectedIndexChanged);
+    }
+
+    protected void Page_PreRender(object sender, EventArgs e) {
+        // Set properties on verbs.
+        connectionsZone1.CancelVerb.Description =
+          "Terminates the connection process";
+        connectionsZone1.CloseVerb.Description =
+          "Closes the connections UI";
+        connectionsZone1.ConfigureVerb.Description =
+          "Configure the transformer for the connection";
+        connectionsZone1.ConnectVerb.Description =
+          "Connect two WebPart controls";
+        connectionsZone1.DisconnectVerb.Description =
+          "End the connection between two controls";
+
+        // Set properties for UI text strings.
+        connectionsZone1.ConfigureConnectionTitle =
+          "Configure";
+        connectionsZone1.ConnectToConsumerInstructionText =
+          "Choose a consumer connection point";
+        connectionsZone1.ConnectToConsumerText =
+          "Select a consumer for the provider to connect with";
+        connectionsZone1.ConnectToConsumerTitle =
+          "Send data to this consumer";
+        connectionsZone1.ConnectToProviderInstructionText =
+          "Choose a provider connection point";
+        connectionsZone1.ConnectToProviderText =
+          "Select a provider for the consumer to connect with";
+        connectionsZone1.ConnectToProviderTitle =
+          "Get data from this provider";
+        connectionsZone1.ConsumersInstructionText =
+          "WebPart controls that receive data from providers";
+        connectionsZone1.ConsumersTitle = "Consumer Controls";
+        connectionsZone1.GetFromText = "Receive from";
+        connectionsZone1.GetText = "Retrieve";
+        connectionsZone1.HeaderText =
+         "Create and Manage Connections";
+        connectionsZone1.InstructionText =
+         "Manage connections for the selected WebPart control";
+        connectionsZone1.InstructionTitle =
+          "Manage connections for consumers or providers";
+        connectionsZone1.NoExistingConnectionInstructionText =
+          "No connections exist. Click the above link to create "
+          + "a connection.";
+        connectionsZone1.NoExistingConnectionTitle =
+          "No current connections";
+        connectionsZone1.ProvidersInstructionText =
+          "WebPart controls that send data to consumers";
+        connectionsZone1.ProvidersTitle = "Provider controls";
+
+    }
+}
