4c402753e569e21af734d37618d635c56bc64b18
Robby Tarigan <robby.tarigan@gmail.com>
2011-12-04 23:16:41 +0000

    Enable translators who are not developers to create local and global resource files. In this chance only for local resources. But the concept is similar to global resource.

---
+++ 70-515/GlobalSite/translator.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="false" CodeFile="translator.aspx.cs" Inherits="TranslatorPage" EnableViewState="false" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Translator for Default Page</h1>
+
+        <asp:DropDownList ID="DropDownListLang" runat="server" AutoPostBack="True" AppendDataBoundItems="true">
+            <asp:ListItem Value="">Choose Language</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+
+        <asp:PlaceHolder runat="server" ID="controlHolder" />  
+
+        <br />
+        <asp:Button Text="Submit" runat="server" ID="submitButton" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ 70-515/GlobalSite/translator.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Globalization;
+using System.Xml.Linq;
+using System.IO;
+
+public partial class TranslatorPage : System.Web.UI.Page
+{
+
+    private string selectedLanguage;
+
+    protected override void OnPreInit(EventArgs e) {
+        base.OnPreInit(e);
+
+        foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.SpecificCultures)) {
+            DropDownListLang.Items.Add(new ListItem(ci.NativeName, ci.Name));
+        }
+    }
+
+    protected override void OnLoad(EventArgs e) {
+        base.OnLoad(e);
+        selectedLanguage = DropDownListLang.SelectedValue;
+    }
+
+    protected override void OnLoadComplete(EventArgs e) {
+        base.OnLoadComplete(e);
+
+        string localResourceFolder = Server.MapPath("App_LocalResources");
+        string resourceFile = String.Format("{0}/Default.aspx{1}.resx", localResourceFolder, String.IsNullOrWhiteSpace(selectedLanguage)?String.Empty:"." + selectedLanguage);
+
+        if (!File.Exists(resourceFile)) {
+            string defaultResourceFile = String.Format("{0}/Default.aspx.resx", localResourceFolder);
+            File.Copy(defaultResourceFile, resourceFile);
+        }
+
+        XDocument doc = XDocument.Load(resourceFile);
+
+        bool anyChanged = false;
+
+        foreach (XElement el in doc.Descendants("data")) {
+            string name = el.Attribute(XName.Get("name")).Value;
+
+            string inputName = String.Format("{0}TextBox", name);
+
+            string value = Request.Form[inputName];
+
+            if (value != null) {
+                anyChanged = true;
+                el.Descendants("value").Single().Value = value;
+            } else {
+                value = el.Descendants("value").Single().Value;
+            }
+
+            controlHolder.Controls.Add(new Label { ID = String.Format("{0}Label", name), Text = name, AssociatedControlID = inputName });
+            controlHolder.Controls.Add(new TextBox { ID = inputName, Text = value });
+            controlHolder.Controls.Add(new Literal { Text = "<br />" });            
+        }
+
+        if (anyChanged) {
+            doc.Save(resourceFile);
+        }
+        
+    }
+
+    
+}
