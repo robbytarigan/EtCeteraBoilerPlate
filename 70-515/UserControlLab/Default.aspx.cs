571b7cd9eb8b411f30ccb30df31ce0ca957f787a
Robby Tarigan <robby.tarigan@gmail.com>
2011-12-05 22:02:08 +0000

    Hosting the user control on a web page.

---
+++ 70-515/UserControlLab/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void AddressUserControl1_SaveButtonClick(object sender, EventArgs e) {
+        System.Diagnostics.Debug.WriteLine("Address: " +
+            AddressUserControl1.Address +
+            " City: " + AddressUserControl1.City +
+            " State or Province: " + AddressUserControl1.StateProvince +
+            " Postal Code: " + AddressUserControl1.PostalCode);
+    }
+
+    
+
+    protected void Page_Init(object sender, EventArgs e) {
+        AddressUserControl1.SaveButtonClick += new EventHandler(AddressUserControl1_SaveButtonClick);
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
