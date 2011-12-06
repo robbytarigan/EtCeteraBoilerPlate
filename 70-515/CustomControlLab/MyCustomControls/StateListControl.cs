60ace99c258050141f737e0f3c74f61a320d2a0c
Robby Tarigan <robby.tarigan@gmail.com>
2011-12-06 23:39:20 +0000

    Creating the Visual Studio Projects and Defining the Custom Web Server Control

---
+++ 70-515/CustomControlLab/MyCustomControls/StateListControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyCustomControls {
+    [ToolboxData("<{0}:StateListControl runat=\"server\" ID=\"stateListControl1\"></{0}:StateListControl>")]
+    public class StateListControl : DropDownList {
+        public StateListControl() {
+            this.Items.Add(new ListItem("Alabama", "AL"));
+            this.Items.Add(new ListItem("Alaska", "AK"));
+            this.Items.Add(new ListItem("Arizona", "AZ"));
+            this.Items.Add(new ListItem("Arkansas", "AR"));
+            this.Items.Add(new ListItem("California", "CA"));
+            //and more ...
+        }
+    }
+}
