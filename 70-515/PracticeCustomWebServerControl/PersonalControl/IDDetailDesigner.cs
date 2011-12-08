84924f4d3a6b0f486f670eb9aa613989c719e807
Robby Tarigan <robby.tarigan@gmail.com>
2011-12-08 21:48:35 +0000

    Create a new custom web server control.

---
+++ 70-515/PracticeCustomWebServerControl/PersonalControl/IDDetailDesigner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI.Design;
+using System.ComponentModel;
+
+namespace PersonalControl {
+    class IDDetailDesigner : ControlDesigner {
+        private IDDetail _idDetail;
+
+        public override string GetDesignTimeHtml() {
+            if (_idDetail.NameLabel == "Name" || _idDetail.NameLabel == String.Empty) {
+                return "<div style=\"color:green;\">[Define NameLabel]</div>";
+            } else {
+                return base.GetDesignTimeHtml();
+            }
+        }
+
+        public override void Initialize(IComponent component) {
+            _idDetail = (IDDetail)component;
+            base.Initialize(component);
+        }
+    }
+}
