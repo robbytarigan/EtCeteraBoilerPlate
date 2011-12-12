680a1f3365a3c227af83d5b94d5af52f20b98fb4
Robby Tarigan <robby.tarigan@gmail.com>
2011-12-12 22:26:53 +0000

    Enabling script debugging.

---
+++ 70-515/ConfiguringAWebsiteForDebugging/ScriptDebug.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ScriptDebug : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        int a = 10;
+        int b = 20;
+
+        Response.Write(String.Format("{0} + {1} = {2}", a, b, a + b));
+    }
+}
