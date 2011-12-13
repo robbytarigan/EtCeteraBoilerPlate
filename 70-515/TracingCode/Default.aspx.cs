48ceb6b8b8c92d0b08b9ef8dc0c761dced8a74ad
Robby Tarigan <robby.tarigan@gmail.com>
2011-12-13 22:30:40 +0000

    Using the ASP .NET trace facility.

---
+++ 70-515/TracingCode/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{    
+
+    protected void Page_PreInit(object sender, EventArgs e) {
+        timeLabel.Text = String.Format("Current server time is {0:dd/MM/yyyy HH:mm:ss}", DateTime.Now);
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Trace.Write("Custom Category", "Page.Load called");
+    }
+}
