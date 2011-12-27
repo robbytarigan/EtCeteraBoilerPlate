3e002d32732d6b00fcbccc7db84fb253b6512cfc
Robby Tarigan <robby.tarigan@gmail.com>
2011-12-27 23:40:47 +0000

    Embedding an Ajax client control as a custom control.

---
+++ 70-515/AjaxEnabled/PassTextBox/PassTextBox.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI.WebControls;
+using System.Web.UI;
+using System.Web;
+
+namespace AjaxEnabled {
+    public class PassTextBox : TextBox, IScriptControl {
+        private ScriptManager sMgr;
+
+        public string WeakCssClass;
+        public string MediumCssClass;
+        public string StrongCssClass;
+
+        IEnumerable<ScriptDescriptor> IScriptControl.GetScriptDescriptors() {
+            ScriptControlDescriptor descriptor = new ScriptControlDescriptor("AjaxEnabled.PassTextBox", this.ClientID);
+
+            descriptor.AddProperty("weakCssClass", this.WeakCssClass);
+            descriptor.AddProperty("mediumCssClass", this.MediumCssClass);
+            descriptor.AddProperty("strongCssClass", this.StrongCssClass);
+
+            return new ScriptDescriptor[] { descriptor };
+        }
+
+        IEnumerable<ScriptReference> IScriptControl.GetScriptReferences() {
+            ScriptReference reference = new ScriptReference();
+            reference.Assembly = "AjaxEnabled";
+            reference.Name = "AjaxEnabled.PassTextBox.js";
+
+            return new ScriptReference[] { reference };
+        }
+
+        protected override void OnPreRender(EventArgs e) {            
+            base.OnPreRender(e);
+
+            if (!this.DesignMode) {
+                // test for the existence of a Script Manager
+                sMgr = ScriptManager.GetCurrent(Page);
+
+                if (sMgr == null)
+                    throw new HttpException("A ScriptManager control must exist on the page.");
+
+                sMgr.RegisterScriptControl(this);
+            }
+        }
+
+        protected override void Render(HtmlTextWriter writer) {
+            base.Render(writer);
+
+            if (!this.DesignMode)
+                sMgr.RegisterScriptDescriptors(this);
+        }
+    }
+}
---
+++ 70-515/AjaxEnabled/PassTextBox/PassTextBox.js
@@ -0,0 +1,83 @@
+﻿/// <reference name="MicrosoftAjax.js" />
+
+Type.registerNamespace("AjaxEnabled");
+
+// create constructor
+
+AjaxEnabled.PassTextBox = function (element) {
+    AjaxEnabled.PassTextBox.initializeBase(this, [element]);
+
+    this._weakCssClass = null;
+    this._mediumCssClass = null;
+    this._strongCssClass = null;
+}
+
+// define class
+AjaxEnabled.PassTextBox.prototype = {
+    //initialize the UI control
+    initialize: function () {
+        AjaxEnabled.PassTextBox.callBaseMethod(this, 'initialize');        
+        this._onKeyupHandler = Function.createDelegate(this, this._onKeyup);
+        $addHandlers(this.get_element(), { 'keyup': this._onKeyup }, this);
+    },
+    dispose: function () {
+        $clearHandlers(this.get_element());
+        AjaxEnabled.PassTextBox.callBaseMethod(this, 'dispose');
+    },
+    //define keystroke event
+    _onKeyup: function (e) {
+        //get password text
+        var pass = this.get_element().value;
+        var strength = this.returnPasswordStrength(pass);
+        switch (strength) {
+            case "Weak":
+                this.get_element().className = this._weakCssClass;
+                break;
+            case "Medium":
+                this.get_element().className = this._mediumCssClass;
+                break;
+            case "Strong":
+                this.get_element().className = this._strongCssClass;
+                break;
+        }
+    },
+    //define properties
+    get_weakCssClass: function () {
+        return this._weakCssClass;
+    },
+    set_weakCssClass: function (value) {
+        this._weakCssClass = value;
+    },
+    get_mediumCssClass: function () {
+        return this._mediumCssClass;
+    },
+    set_mediumCssClass: function (value) {
+        this._mediumCssClass = value;
+    },
+    get_strongCssClass: function () {
+        return this._strongCssClass;
+    },
+    set_strongCssClass: function (value) {
+        this._strongCssClass = value;
+    },
+    returnPasswordStrength: function (password) {
+        var strPass = new String(password.toString());
+        if (strPass.length < 5) {
+            return "Weak";
+        }
+        else {
+            if (strPass.length < 8) {
+                return "Medium";
+            }
+            else {
+                return "Strong";
+            }
+        }
+    }
+}
+
+// register class as a Sys.Control
+AjaxEnabled.PassTextBox.registerClass('AjaxEnabled.PassTextBox', Sys.UI.Control);
+
+// notify load
+if (typeof (Sys) !== 'undefined') Sys.Application.notifyScriptLoaded();
