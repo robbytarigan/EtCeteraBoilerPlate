9670677dae0e339b78666159a4938736dc79c5b2
Robby Tarigan <robby.tarigan@gmail.com>
2012-01-18 23:17:01 +0000

    Create website and a custom HTTP handler to return images based on user requests.

---
+++ 70-515/JpegHandler/App_Code/JpegHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for JpegHandler
+/// </summary>
+public class JpegHandler : IHttpHandler
+{
+	public JpegHandler()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    bool IHttpHandler.IsReusable {
+        get { return false; }
+    }
+
+    void IHttpHandler.ProcessRequest(HttpContext context) {
+        context.Response.ContentType = "image/jpeg";
+
+        HttpRequest Request = context.Request;
+        HttpResponse Response = context.Response;
+
+        if (Request.RawUrl.ToLowerInvariant().Contains("jellyfish.jpg")) {
+            Response.TransmitFile(Request.PhysicalApplicationPath + "/Images/Jellyfish.jpg");
+        } else if (Request.RawUrl.ToLowerInvariant().Contains("koala.jpg")) {
+            Response.TransmitFile(Request.PhysicalApplicationPath + "/Images/Koala.jpg");
+        } else {
+            Response.Write("File not found");
+        }
+    }
+}
