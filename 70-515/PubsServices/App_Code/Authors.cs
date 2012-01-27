e6de7191a00a56d2bee1053008a811d4ccc01e39
Robby Tarigan <robby.tarigan@gmail.com>
2012-01-27 22:36:33 +0000

    Creating and consuming ASP .NET web service.

---
+++ 70-515/PubsServices/App_Code/Authors.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Text;
+
+/// <summary>
+/// Summary description for Authors
+/// </summary>
+[WebService(Namespace = "http://rstarigan.com/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+public class Authors  {
+    private string _cnnString = ConfigurationManager.ConnectionStrings["PubsConnectionString"].ToString();
+
+    public Authors () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod(CacheDuration = 300)]
+    public DataTable GetAuthorTitles(string authorId) {
+        StringBuilder sql = new StringBuilder();
+        sql.Append("SELECT titles.title, titles.type, titles.price, ");
+        sql.Append("titles.pubdate FROM titleauthor INNER JOIN titles ON ");
+        sql.Append("titleauthor.title_id = titles.title_id ");
+        if (authorId != "0")
+            sql.Append("WHERE (titleauthor.au_id = @AuthorId) ");
+        SqlConnection cnn = new SqlConnection(_cnnString);
+        SqlCommand cmd = new SqlCommand(sql.ToString(), cnn);
+        cmd.Parameters.Add("AuthorId", SqlDbType.VarChar, 11).Value = authorId;
+        SqlDataAdapter adp = new SqlDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        adp.Fill(ds);
+        return ds.Tables[0];
+    }
+}
---
+++ 70-515/PubsServices/PubsClient/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:ObjectDataSource ID="ObjectDataSource1" runat="server" SelectMethod="GetAuthorTitles" TypeName="PubsService.Authors">
+            <SelectParameters>
+                <asp:QueryStringParameter Name="authorId" QueryStringField="auId" Type="String" DefaultValue="0" />
+            </SelectParameters>
+        </asp:ObjectDataSource>
+        <asp:GridView ID="GridView1" runat="server" DataSourceID="ObjectDataSource1">
+        </asp:GridView>
+    
+    </div>
+    </form>
+</body>
+</html>
