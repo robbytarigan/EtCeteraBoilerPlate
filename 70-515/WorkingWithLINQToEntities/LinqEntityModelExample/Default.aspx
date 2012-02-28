a78ef59cbab9e4c394399d6c3d17047fd372800d
Robby Tarigan <robby.tarigan@gmail.com>
2012-02-28 22:27:42 +0000

    Writing the LINQ queries and connecting to a webpage.

---
+++ 70-515/WorkingWithLINQToEntities/LinqEntityModelExample/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h2>
+            View Publisher Titles</h2>
+        Select a publisher from the list:<br />
+        <asp:DropDownList ID="DropDownListPublishers" runat="server">
+        </asp:DropDownList>
+        <asp:Button ID="ButtonShowTitles" runat="server" Text="Show Titles" OnClick="ButtonShowTitles_Click" />
+        <hr />
+        <asp:GridView ID="GridViewTitles" runat="server">
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 70-515/WorkingWithLINQToEntities/LinqEntityModelExample/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack) {
+            PubsEntityModel.PubsEntities pubs = new PubsEntityModel.PubsEntities();
+
+            var pubQuery = from p in pubs.Publishers
+                            orderby p.PublisherName
+                            select p;
+
+            DropDownListPublishers.DataTextField = "PublisherName";
+            DropDownListPublishers.DataValueField = "PublisherId";
+            DropDownListPublishers.DataSource = pubQuery;
+            DropDownListPublishers.DataBind();
+        }
+    }
+
+    protected void ButtonShowTitles_Click(object sender, EventArgs e) {
+        using (PubsEntityModel.PubsEntities pubs = new PubsEntityModel.PubsEntities()) {
+            var titleQuery = from t in pubs.Titles
+                             where t.PublisherId == DropDownListPublishers.SelectedValue
+                             orderby t.TitleName
+                             select t;
+
+            GridViewTitles.DataSource = titleQuery;
+            GridViewTitles.DataBind();
+        }
+    }
+}
